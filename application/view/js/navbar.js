4be5095fcf7c6c85743300d93e8b3488f190b6af
AlexWolski <alex.theinfinite@gmail.com>
2020-01-07 22:16:21 -0800

    Styled navbar and loaded it in index

---
+++ application/view/js/navbar.js
@@ -0,0 +1,14 @@
+function loadNavbar() {
+  var xmlReq= new XMLHttpRequest();
+
+  xmlReq.onreadystatechange= function() {
+    if (this.readyState !== 4)
+      return
+
+  	//Load the navbar
+    document.getElementById('navbar-placeholder').innerHTML= this.responseText;
+  }
+  
+  xmlReq.open('GET', 'navbar.html', true);
+  xmlReq.send();
+}
