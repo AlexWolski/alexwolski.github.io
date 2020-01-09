049dc6a956e487285a8fc1f962e079d48da861b8
AlexWolski <alex.theinfinite@gmail.com>
2020-01-08 22:54:09 -0800

    Adjusted content & footer positioning and added copied text

--- application/view/js/copyEmail.js
+++ application/view/js/copyEmail.js
@@ -1,3 +1,29 @@
+// Copied from hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
 function copyEmail() {
-	alert("copied!");
+  //Create a textarea, populate it with the email, and hide it
+  const textArea = document.createElement('textarea');
+  textArea.value = 'wolski.alexander.john@gmail.com';
+  textArea.setAttribute('readonly', '');
+  textArea.style.position = 'absolute';
+  textArea.style.left = '-9999px';
+  document.body.appendChild(textArea);
+
+  //Copy the contents of the textarea
+  textArea.select();
+  document.execCommand('copy');
+  //Remove the textarea from the document
+  document.body.removeChild(textArea);
+  
+  //Display a message that the email has been copied
+  const copiedPopup = document.getElementById('copied-popup');
+  copiedPopup.className = 'fadeIn';
+  setTimeout(function(){
+    sleep(1000);
+    copiedPopup.className = 'fadeOut';
+  }, 500);
+
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
