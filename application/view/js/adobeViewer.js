68b56c3cf55335103dcbbe05682f20238ddbcbac
AlexWolski <alex.theinfinite@gmail.com>
2020-02-22 00:37:57 -0800

    Changed PDF view to inline mode

--- application/view/js/adobeViewer.js
+++ application/view/js/adobeViewer.js
@@ -1,11 +1,11 @@
 //Use the AdobeDCView API to create a pdf viewer element in the given div with the given file
-function viewPDF(div, file, fileName) {
+function viewPDF(div, embedMode, file, fileName) {
   document.addEventListener("adobe_dc_view_sdk.ready", function() {
     var adobeDCView = new AdobeDC.View({clientId: "41cb26db63cd42288135f1c23a0dd726", divId: div});
     
     adobeDCView.previewFile({
       content:{location: {url: file}},
       metaData:{fileName: fileName}},
-      {embedMode: "SIZED_CONTAINER"});
+      {embedMode: embedMode});
   });
 }
