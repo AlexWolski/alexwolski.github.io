21725d07053e1fb0daab3312ee2b5d1bb95c866a
AlexWolski <alex.theinfinite@gmail.com>
2020-02-22 14:36:17 -0800

    Removed unneeded .less imports

--- application/view/js/profilePicture.js
+++ application/view/js/profilePicture.js
@@ -1,11 +1,11 @@
 // Modified from https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
 
 $(document).ready(function(){
-	$('head style[type="text/css"]').attr('type', 'text/less');
-	less.refreshStyles();
-	window.load = function() {
-		$('.radial-progress').attr('data-progress', 100);
-		$('.profile-picture').addClass('fade-in-pfp');
-	}
-	setTimeout(window.load);
+  $('head style[type="text/css"]').attr('type', 'text/less');
+  less.refreshStyles();
+  window.load = function() {
+    $('.radial-progress').attr('data-progress', 100);
+    $('.profile-picture').addClass('fade-in-pfp');
+  }
+  setTimeout(window.load);
 })
