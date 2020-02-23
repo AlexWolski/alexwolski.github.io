7f4388e09164ccc1cf5dd2caaa7812e415bfd2b1
AlexWolski <alex.theinfinite@gmail.com>
2020-02-22 16:33:32 -0800

    Imported Adobe script through js to make pdf more reliable

--- application/view/js/adobeViewer.js
+++ application/view/js/adobeViewer.js
@@ -1,5 +1,7 @@
 //Use the AdobeDCView API to create a pdf viewer element in the given div with the given file
 function viewPDF(div, embedMode, file, fileName) {
+  importAdobeSDK();
+
   document.addEventListener("adobe_dc_view_sdk.ready", function() {
     var adobeDCView = new AdobeDC.View({clientId: "41cb26db63cd42288135f1c23a0dd726", divId: div});
 
@@ -10,4 +12,12 @@ function viewPDF(div, embedMode, file, fileName) {
 
     adobeDCView.setResizable(true);
   });
+}
+
+//Import the adobe SDK script
+function importAdobeSDK()
+{
+  var imported = document.createElement("script");
+  imported.src = "https://documentcloud.adobe.com/view-sdk/main.js";
+  document.getElementsByTagName("head")[0].appendChild(imported);
 }
