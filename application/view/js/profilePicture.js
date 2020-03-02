5d21388538040d0e23ded6d4dc99b96bf3934c41
AlexWolski <alex.theinfinite@gmail.com>
2020-03-01 16:17:58 -0800

    Created a gulp task to convert .less to .css

--- application/view/js/profilePicture.js
+++ application/view/js/profilePicture.js
@@ -1,8 +1,7 @@
 // Modified from https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
 
 $(document).ready(function(){
-  $('head style[type="text/css"]').attr('type', 'text/less');
-  less.refreshStyles();
+  $('head style[type="text/css"]').attr('type', 'text/css');
   window.load = function() {
     $('.radial-progress').attr('data-progress', 100);
     $('.profile-picture').addClass('fade-in-pfp');
